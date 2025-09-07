7fa2a02e0175dcf69b79ede92a76367f35f5c617
Rama Narasimhan <rnarasimhan4@gmail.com>
2025-09-06 21:34:31 -0700

    improve responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -18,14 +18,23 @@ function App() {
 
   return (
     <div className={`h-screen ${theme === 'dark' ? 'theme-dark' : 'theme-light'} flex flex-col overflow-hidden`}>
-      <Header />
-      <div className="flex-1 flex">
-        <div className="flex-1 flex flex-col">
+      <div className="shrink-0">
+        <Header />
+      </div>
+
+      <div className="flex-1 flex overflow-hidden">
+        <div className="flex-1 min-w-0 overflow-hidden">
           <PokerTable />
         </div>
-        <Sidebar />
+
+        <div className="w-80 shrink-0">
+          <Sidebar />
+        </div>
       </div>
-      <NotificationCenter />
+
+      <div className="fixed bottom-4 right-4 z-50">
+        <NotificationCenter />
+      </div>
     </div>
   );
 }
